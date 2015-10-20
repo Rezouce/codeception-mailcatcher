8a7b12b8d2516c33395e1d222261a55e37cc4ee7
Rezouce <contact@rezouce.net>
2015-10-20 15:18:36 +0200

    Create module class.

---
+++ src/MailCatcher.php
@@ -0,0 +1,188 @@
+<?php
+namespace Codeception\Module;
+
+use Codeception\Module;
+use Codeception\TestCase;
+use Guzzle\Http\Client;
+use MailCatcher\Mail;
+use MailCatcher\MailCatcherAdapter;
+use MailCatcher\MailCollection;
+
+class MailCatcher extends Module
+{
+
+    private $url = '127.0.0.1';
+
+    private $port = '1080';
+
+    private $resetBeforeEachTest = false;
+
+    private $mailCatcher;
+
+    private $emails;
+
+    /**
+     * Initialize from configuration.
+     */
+    public function _initialize()
+    {
+        if (isset($this->config['url'])) {
+            $this->url = trim($this->config['url'], '/');
+        }
+
+        if (isset($this->config['port'])) {
+            $this->port = $this->config['port'];
+        }
+
+        if (isset($this->config['resetBeforeEachTest'])) {
+            $this->resetBeforeEachTest = $this->config['resetBeforeEachTest'];
+        }
+    }
+
+    /**
+     * Drop the previous email from MailCatcher if not disabled by configuration.
+     *
+     * @param TestCase $test
+     */
+    public function _before(TestCase $test)
+    {
+        if ($this->resetBeforeEachTest) {
+            $this->resetEmails();
+        }
+    }
+
+    /**
+     * Guzzle client to talk with MailCatcher.
+     *
+     * @return \MailCatcher\MailCatcher
+     */
+    private function mailCatcher()
+    {
+        if (null === $this->mailCatcher) {
+            $this->mailCatcher = new \MailCatcher\MailCatcher(
+                new MailCatcherAdapter(new Client, $this->url . ':' . $this->port)
+            );
+        }
+
+        return $this->mailCatcher;
+    }
+
+    /**
+     * Get all messages from MailCatcher.
+     *
+     * @return MailCollection
+     */
+    private function emails()
+    {
+        if (null === $this->emails) {
+            $this->emails = $this->mailCatcher()->messages();
+        }
+
+        return $this->emails;
+    }
+
+    /**
+     * Remove all emails in MailCatcher.
+     */
+    public function removeEmails()
+    {
+        $this->mailCatcher()->removeMessages();
+    }
+
+    /**
+     * Assert that there is at least one email.
+     * @param MailCollection $emails
+     */
+    public function hasEmails(MailCollection $emails = null)
+    {
+        if (null === $emails) {
+            $emails = $this->emails();
+        }
+
+        $this->assertNotEmpty($emails->count(), 'Has an email');
+    }
+
+    /**
+     * Assert the number of emails sent.
+     *
+     * @param int $count
+     * @param MailCollection $emails
+     */
+    public function seeNumberEmails($count, MailCollection $emails = null)
+    {
+        if (null === $emails) {
+            $emails = $this->emails();
+        }
+
+        $this->assertEquals($count, $emails->count(), 'Number of emails');
+    }
+
+    /**
+     * Assert that at least an email contains some text.
+     *
+     * @param string $expected
+     * @param MailCollection $emails
+     */
+    public function seeInEmail($expected, MailCollection $emails = null)
+    {
+        if (null === $emails) {
+            $emails = $this->emails();
+        }
+
+        $emails = $emails->filter(function(Mail $mail) use ($expected) {
+            return false !== strstr($mail->source(), $expected);
+        });
+
+        $this->assertNotEmpty($emails->count(), 'Email contains');
+    }
+
+    /**
+     * Get all emails with the given subject.
+     *
+     * @param string $subject
+     * @param bool $strict
+     * @return MailCollection
+     */
+    public function getEmailsBySubject($subject, $strict = false)
+    {
+        return $this->emails()->filter(function(Mail $mail) use ($subject, $strict) {
+            return $strict
+                ? $subject === $mail->subject()
+                : false !== strstr($mail->subject(), $subject);
+        });
+    }
+
+    /**
+     * Get all emails with the given sender.
+     *
+     * @param string $sender
+     * @param bool $strict
+     * @return MailCollection
+     */
+    public function getEmailsBySender($sender, $strict = false)
+    {
+        return $this->emails()->filter(function(Mail $mail) use ($sender, $strict) {
+            return $strict
+                ? $sender === $mail->sender()
+                : false !== strstr($mail->sender(), $sender);
+        });
+    }
+
+    /**
+     * Get all emails with the given recipients.
+     *
+     * @param array $recipients
+     * @param bool $strict
+     * @return MailCollection
+     */
+    public function getEmailsByRecipients(array $recipients, $strict = false)
+    {
+        return $this->emails()->filter(function(Mail $mail) use ($recipients, $strict) {
+            $found = count(array_intersect($recipients, $mail->recipients()));
+
+            return $strict
+                ? $found === count($recipients)
+                : $found > 0;
+        });
+    }
+}
---
+++ tests/functional/MailCatcherTest.php
@@ -0,0 +1,110 @@
+<?php
+
+
+class MailCatcherTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \FunctionalTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+//        $this->removeEmails();
+    }
+
+    /** @test */
+    public function it_count_the_emails()
+    {
+        mail('user@example.com', 'Subject', 'Body.');
+        mail('user@example.com', 'Subject', 'Body.');
+        mail('user@example.com', 'Subject', 'Body.');
+
+        $this->seeNumberEmails(3);
+    }
+
+    /** @test */
+    public function it_remove_the_emails()
+    {
+        mail('user@example.com', 'Subject', 'Body.');
+        $this->seeNumberEmails(1);
+
+        $this->removeEmails();
+        $this->seeNumberEmails(0);
+    }
+
+    /** @test */
+    public function it_has_emails()
+    {
+        mail('user@example.com', 'Subject', 'Body.');
+
+        $this->seeHasEmails();
+    }
+
+    /** @test */
+    public function it_has_emails_which_contain_a_stringin_its_source()
+    {
+        mail('user@example.com', 'Subject', 'Body.');
+
+        $this->seeInEmail('example');
+    }
+
+    /** @test */
+    public function it_retrieve_the_emails_by_their_subject()
+    {
+        mail('user@example.com', 'Subject 1', 'Body.');
+        mail('user@example.com', 'Subject 2', 'Body.');
+        mail('user@example.com', 'Other name', 'Body.');
+
+        $this->assertEquals(2, $this->getEmailsBySubject('Subject')->count());
+    }
+
+    /** @test */
+    public function it_retrieve_the_emails_by_their_subject_strictly()
+    {
+        mail('user@example.com', 'Subject 1', 'Body.');
+        mail('user@example.com', 'Subject 2', 'Body.');
+        mail('user@example.com', 'Subject', 'Body.');
+
+        $this->assertEquals(1, $this->getEmailsBySubject('Subject', true)->count());
+    }
+
+    /** @test */
+    public function it_retrieve_the_emails_by_their_sender()
+    {
+        mail('user@example.com', 'Subject', 'Body.', 'From: user@example.com');
+        mail('user@example.com', 'Subject', 'Body.', 'From: user@example.com');
+        mail('user@example.com', 'Subject', 'Body.', 'From: user@other.com');
+
+        $this->assertEquals(2, $this->getEmailsBySender('example.com')->count());
+    }
+
+    /** @test */
+    public function it_retrieve_the_emails_by_their_sender_strictly()
+    {
+        mail('user@example.com', 'Subject', 'Body.', 'From: user@example.coma');
+        mail('user@example.com', 'Subject', 'Body.', 'From: user@example.comb');
+        mail('user@example.com', 'Subject', 'Body.', 'From: user@example.com');
+
+        $this->assertEquals(1, $this->getEmailsBySender('user@example.com', true)->count());
+    }
+
+    /** @test */
+    public function it_retrieve_the_emails_by_their_recipients()
+    {
+        mail('user1@example.com, user2@example.com, user3@example.com', 'Subject', 'Body.');
+        mail('user1@example.com, user4@example.com', 'Subject', 'Body.');
+        mail('user4@example.com', 'Subject', 'Body.');
+
+        $this->assertEquals(2, $this->getEmailsByRecipients(['user1@example.com', 'user2@example.com'])->count());
+    }
+
+    /** @test */
+    public function it_retrieve_the_emails_by_their_recipients_strictly()
+    {
+        mail('user1@example.com, user2@example.com, user3@example.com', 'Subject', 'Body.');
+        mail('user1@example.com, user2@example.com', 'Subject', 'Body.');
+
+        $this->assertEquals(1, $this->getEmailsByRecipients(['user1@example.com', 'user2@example.com'], true)->count());
+    }
+}
